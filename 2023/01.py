95ece96f311acbdd6b2f44b448b0c9146dba8707
Paul Géhin <paul.gehin@insee.fr>
2023-12-01 17:57:32 +0100

    01/12/2023

--- 2023/01.py
+++ 2023/01.py
@@ -1,21 +1,52 @@
 def prepare_data(data, test=False):
-    return list(map(lambda x: int(x), data))
+    if test:
+        i = data.index("")
+        return [data[:i], data[i + 1:]]
+    return data
+
+
+digits = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+reversed_digits = {k[::-1]: v for k, v in digits.items()}
+
+
+def find_digit(line: str, catalog: dict[str, int] | None = None) -> int:
+    catalog = {} if catalog is None else catalog
+    for i, char in enumerate(line):
+        if char.isdigit():
+            return int(char)
+        for k, v in catalog.items():
+            if line[i:].startswith(k):
+                return v
 
 
 def resu1(data):
-    return None
+    if len(data) == 2:
+        return sum(find_digit(line) * 10 + find_digit(line[::-1]) for line in data[0])
+    return sum(find_digit(line) * 10 + find_digit(line[::-1]) for line in data)
 
 
 def resu2(data):
-    return None
+    if len(data) == 2:
+        return sum(find_digit(line, digits) * 10 + find_digit(line[::-1], reversed_digits) for line in data[1])
+    return sum(find_digit(line, digits) * 10 + find_digit(line[::-1], reversed_digits) for line in data)
 
 
 def test1(resu):
-    return False
+    return resu == 142
 
 
 def test2(resu):
-    return False
+    return resu == 281
 
 
 if __name__ == "__main__":
